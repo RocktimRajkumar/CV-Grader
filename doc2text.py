3c94040875942bfb3cb27fbda56ace5bb116649b
RocktimRajkumar <rajkumar.rocktim@gmail.com>
2020-03-27 21:50:57 +0530

    feat: doc2text.py added

---
+++ doc2text.py
@@ -0,0 +1,8 @@
+import docx2txt
+
+def extract_text_from_doc(doc_path):
+    temp = docx2txt.process(doc_path)
+    text = [line.replace('\t', ' ') for line in temp.split('\n') if line]
+    return ' '.join(text)
+
+print(extract_text_from_doc("resumes/resume.docx"))
