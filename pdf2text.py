0e454dba5c187b30b1b307a5edd202f2b6bee750
RocktimRajkumar <rajkumar.rocktim@gmail.com>
2020-03-27 21:45:52 +0530

    feat: pdf2text.py added

---
+++ pdf2text.py
@@ -0,0 +1,49 @@
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import io
+
+
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as fh:
+        # iterate over all pages of PDF document
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+            # creating a resoure manager
+            resource_manager = PDFResourceManager()
+
+            # create a file handle
+            fake_file_handle = io.StringIO()
+
+            # creating a text converter object
+            converter = TextConverter(
+                resource_manager,
+                fake_file_handle,
+                codec='utf-8',
+                laparams=LAParams()
+            )
+
+            # creating a page interpreter
+            page_interpreter = PDFPageInterpreter(
+                resource_manager,
+                converter
+            )
+
+            # process current page
+            page_interpreter.process_page(page)
+
+            # extract text
+            text = fake_file_handle.getvalue()
+            yield text
+
+            # close open handles
+            converter.close()
+            fake_file_handle.close()
+
+
+text = ""
+# calling above function and extracting text
+for page in extract_text_from_pdf("./resumes/invoice.pdf"):
+    text += ' ' + page
+
+print(text)
